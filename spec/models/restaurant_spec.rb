6d1acaae4b3b4c6060499c6415da4ed61ad3aec3
Alexander Person <alexander@Leonards-MacBook-Pro.local>
2015-10-13 12:03:04 +0100

    tied the destroy commands of restaurants and reviews together

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Restaurant, type: :model  do
+	it { is_expected.to have_many(:reviews).dependent(:destroy) }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,3 +3,7 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe Review, type: :model do
+	it { is_expected.to belong_to(:restaurant) }
+end
