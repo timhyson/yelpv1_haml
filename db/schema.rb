0b2444db902869281d80037acfc92265ca0fa6a8
Tim Hyson <tjhyson@gmail.com>
2015-10-14 10:37:06 +0100

    styling for error changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012171522) do
+ActiveRecord::Schema.define(version: 20151012172914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
